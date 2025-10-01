1021ec6dc7db843230b574db191db1a9a56e602e
natnael.engeda <natnael.engeda@partner.safaricom.et>
2025-10-01 10:22:35 +0300

    update image config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       new URL('https://example.com/thumbnails/**'),
-      new URL('http://localhost:6413/image/**')
+      new URL('http://localhost:6413/image/**'),
+      new URL('https://gumball-backend.alamondaii.com/image/**')
     ],
   },
 };
