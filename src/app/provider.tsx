ed450b28fa150b66cccb0d9dbbbc6fbde714fced
natnael.engeda <natnael.engeda@partner.safaricom.et>
2025-10-01 11:51:29 +0300

    Add Firebase

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,13 +1,28 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { analytics, firebase } from "@/utils/firebase";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient());
+
+  // Initialize Firebase
+  const InitializeFirebase = () => {
+    try {
+      firebase
+      analytics
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    InitializeFirebase();
+  }, []);
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
